f31f3e1d7ccc249ce45b0f4853b448aea593a0bd
Rich Long <rich@bipsync.com>
2018-07-19 13:40:19 +0100

    Initial Commit

---
+++ GenericDataSourceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericDataSourceExample
+//
+//  Created by Rich Long on 19/07/2018.
+//  Copyright © 2018 Bipsync LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
